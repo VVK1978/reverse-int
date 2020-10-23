fd2ae1bd91a16fa30ae89758067bd3726b95b8d1
VVK1978 <70840645+VVK1978@users.noreply.github.com>
2020-10-23 19:01:22 +0300

    Solution reverse-int task

--- src/1.js
+++ src/1.js
@@ -1,15 +1,21 @@
-module.exports = function reverse (n) {
+function reverse (n) {
    arr = []
    if (n<0){
       newN=n.toString().slice(1,n.length)
+      console.log(newN)
       for (i=0;i<newN.length;i++){
          arr.push(+newN.charAt(i))
       }
-      return arr.reverse().join('');
+      return '-'+arr.reverse().join('');
    }else
   newN = n.toString();
 for (i=0;i<newN.length;i++){
    arr.push(+newN.charAt(i))
 }
 return arr.reverse().join('');
+  
+     
    }
+   n= 53320;
+   console.log(reverse(n))
+   
